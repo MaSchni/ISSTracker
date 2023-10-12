3784ebe49c374196b18955ded4037809436d412a
Marco <118534021+MaSchni@users.noreply.github.com>
2023-10-12 13:49:24 +0200

    Add http request to get ISS data

--- app/src/main/java/ch/ost/rj/mge/isstracker/MainActivity.kt
+++ app/src/main/java/ch/ost/rj/mge/isstracker/MainActivity.kt
@@ -1,13 +1,20 @@
 package ch.ost.rj.mge.isstracker
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ch.ost.rj.mge.isstracker.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +38,28 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        getISSData();
+
+    }
+
+    private fun getISSData(){
+        val request = Request.Builder()
+            .url("http://api.open-notify.org/iss-now.json?callback=?")
+            .build()
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    Log.d("json", response.body!!.string())
+                }
+            }
+        })
     }
 }
