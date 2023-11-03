8c21d1c2a71efea57d8097fa42fc492898a674a8
Marco <118534021+MaSchni@users.noreply.github.com>
2023-11-03 14:57:43 +0100

    Changed Notifications-Tab to Settings-Tab

--- app/src/main/java/ch/ost/rj/mge/isstracker/ui/notifications/SettingsFragment.kt
+++ app/src/main/java/ch/ost/rj/mge/isstracker/ui/notifications/SettingsFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ch.ost.rj.mge.isstracker.databinding.FragmentNotificationsBinding
+import ch.ost.rj.mge.isstracker.databinding.FragmentSettingsBinding
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,15 +22,12 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
---
+++ app/src/main/java/ch/ost/rj/mge/isstracker/ui/notifications/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package ch.ost.rj.mge.isstracker.ui.notifications
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+}
